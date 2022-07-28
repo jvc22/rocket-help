7bd56fa842750710b67d91d0da54634563147959
João Victor <105883612+jvc22@users.noreply.github.com>
2022-07-28 15:40:24 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# rocket-help
+ App project for interactions and technical requests within an institution - Ignite Lab (Rocketseat).
---
+++ rockethelp/src/routes/index.tsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native'
+import { Loading } from '../components/Loading';
+import { SignIn } from '../screens/SignIn'
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+import { AppRoutes } from './app.routes';
+
+export function Routes(){
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [user, setUser] = useState<FirebaseAuthTypes.User>();
+
+    useEffect(() => {
+        const subscriber = auth().onAuthStateChanged(response => {
+            setUser(response);
+            setIsLoading(false);
+        });
+
+        return subscriber;
+    },[]);
+
+    if(isLoading) {
+        return <Loading />
+    }
+
+    return(
+        <NavigationContainer>
+            {user ? <AppRoutes /> : <SignIn />}
+        </NavigationContainer>
+    )
+}
